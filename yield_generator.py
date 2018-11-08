aa298eaeeed15cb1394ca8bd1dabcc483113c8e6
brooks <myt2000@126.com>
2018-11-08 22:22:45 +0800

    yield_practice

---
+++ yield_generator.py
@@ -0,0 +1,39 @@
+import os
+import fnmatch
+import sys
+
+def find_files(topdir, pattern):
+    for path, dirname, filelist in os.walk(topdir):
+        for name in filelist:
+            if fnmatch.fnmatch(name, pattern):
+                yield os.path.join(path, name)
+
+import gzip, bz2
+def opener(filenames):
+    for name in filenames:
+        if name.endswith(".gz"):
+            f = gzip.open(name)
+        elif name.endswith(".bz2"):
+            f = bz2.BZ2File
+        else:
+            f = open(name)
+        yield f
+
+def cat(filelist):
+    for f in filelist:
+        for line in f:
+            yield line
+
+def grep(pattern, lines):
+    for line in lines:
+        if pattern in line:
+            yield line
+
+
+if __name__ == "__main__":
+    wwwlogs = find_files("www","access-log")
+    files = opener(wwwlogs)
+    lines = cat(files)
+    pylines = grep("python", lines)
+    for line in pylines:
+        sys.stdout.write(line)
