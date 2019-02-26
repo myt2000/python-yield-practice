f834c6e4af784a9dc93ebecb5d34c1856c8c8a02
MENGYINGTIAN\Administrator <yingtian.meng@gesion.net>
2019-02-26 10:37:57 +0800

    path test

---
+++ path_practice/path_test.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+def get_path():
+    p = Path('.')
+    dir_list = [x for x in p.iterdir() if x.is_dir()]
+    print(dir_list)
+
+if __name__ == '__main__':
+    get_path()
