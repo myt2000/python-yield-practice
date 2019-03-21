e46dfc407e14033577e6fd44683d5ad86de96516
MENGYINGTIAN\Administrator <yingtian.meng@gesion.net>
2019-03-21 14:51:49 +0800

    sqlite3 test python authority file code

---
+++ pickle_practice/only_builtins.py
@@ -0,0 +1,28 @@
+import builtins
+import io
+import pickle
+
+
+#unpickler中只允许加载builtins模块
+safe_builtins = {
+    'range',
+    'complex',
+    'set',
+    'frozenset',
+    'slice',
+}
+
+
+class RestrictedUnpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        # Only allow safe classes from builtins.
+        if module == "builtins" and name in safe_builtins:
+            return getattr(builtins, name)
+        # Forbid everything else.
+        raise pickle.UnpicklingError("global '%s.%s' is forbidden" %
+                                     (module, name))
+
+
+def restricted_loads(s):
+    """Helper function analogous to pickle.loads()."""
+    return RestrictedUnpickler(io.BytesIO(s)).load()
