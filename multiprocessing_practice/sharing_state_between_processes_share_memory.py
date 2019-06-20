12807a947b5e4f9a4e36e6181e892e16280698b3
MENGYINGTIAN\Administrator <yingtian.meng@gesion.net>
2019-06-20 10:34:30 +0800

    multiprocessing Value Array Dict List Pool

---
+++ multiprocessing_practice/sharing_state_between_processes_share_memory.py
@@ -0,0 +1,20 @@
+# 进程间共享状态
+from multiprocessing import Process, Value, Array
+
+
+# 使用Value,Array将数据存储在共享内存映射中
+def f(n, a):
+    n.value = 3.1415927
+    for i in range(len(a)):
+        a[i] = -a[i]
+
+if __name__ == '__main__':
+    num = Value('d', 0.0)
+    arr = Array('i', range(10))
+
+    p = Process(target=f, args=(num, arr))
+    p.start()
+    p.join()
+
+    print(num.value)
+    print(arr[:])
